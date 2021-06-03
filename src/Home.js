b30da8dbe9a3e68e9cae2a706f349c48b0e80d0f
karthi <karthi@exm.com>
2021-06-03 13:25:44 +0530

    route params

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,15 @@
-import React, {useState} from 'react';
+import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'karthik', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'rakshith', id: 3 }
-    ]);
+    const { data: blogs, isPending, Error } = useFetch('http://localhost:8000/blogs');
+
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+           { error && <div>{ error }</div> }
+           { isPending && <div>Loading...</div> }
+           {blogs && <BlogList blogs={blogs} title="All blogs!" />}
         </div>
     );
 }
